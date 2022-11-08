a9fd00e2f6e987b1035bdd002fe8ffd4e460788f
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2022-11-08 21:42:53 +0545

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,85 @@
+var youtube = "https://www.youtube.com/";
+var twitter = "https://www.twitter.com/";
+var password = "code123";
+var linkedin = "https://www.linkedin.com/";
+var instagram = "https://www.instagram.com/";
+var github = "https://github.com/vedanta363/";
+var email = 'mailto:vedant@ldgroups.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Vedant!",
+  "I possess a range of skills in installation, configuration,",
+  "upgrade and maintenance of Information Technology infrastructure. ",
+  "I have 3 years of experience working across different platforms,",
+  "such as Windows Server, Linux Server , VMWare ESXi,",
+  "CISCO Router & Switch, Mikrotik Router, MAIPU Switch, Router, along with",
+  "troubleshooting hardware. I am committed to finding innovative solutions",
+  "to a variety of IT problems, such as security and technical",
+  "troubleshooting. I also possess Computer troubleshooting.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Vedant?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Vedant Agrawal | Proven Success in Systems Optimization and Strategic IT Solutions </span>',
+  "    ",
+  "    ",
+"  ██╗   ██╗███████╗██████╗  █████╗ ███╗   ██╗████████╗███████╗    ████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██╗     ",
+"  ██║   ██║██╔════╝██╔══██╗██╔══██╗████╗  ██║╚══██╔══╝██╔════╝    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██║     ",
+"  ██║   ██║█████╗  ██║  ██║███████║██╔██╗ ██║   ██║   ███████╗       ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║███████║██║     ",
+"  ╚██╗ ██╔╝██╔══╝  ██║  ██║██╔══██║██║╚██╗██║   ██║   ╚════██║       ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██╔══██║██║     ",
+"   ╚████╔╝ ███████╗██████╔╝██║  ██║██║ ╚████║   ██║   ███████║       ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║███████╗",
+"    ╚═══╝  ╚══════╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝  ",                                                                                                                                 
+                                                                                                                   
+  "                                                                                                                          ©2022 ;)",
+  "    ",
+  "    ",
+  "    ", 
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
